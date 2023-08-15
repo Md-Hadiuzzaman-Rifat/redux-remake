13753ecc660a68ac92ecea8b1ef8c45a094a2911
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-08-16 04:37:26 +0600

    problem in state management

---
+++ src/assets/components/TotalCount/TotalCount.jsx
@@ -0,0 +1,11 @@
+
+const TotalCount = ({totalCount}) => {
+    
+    return (
+        <div>
+            <h2>Total Count: {totalCount()}</h2>
+        </div>
+    );
+};
+
+export default TotalCount;
