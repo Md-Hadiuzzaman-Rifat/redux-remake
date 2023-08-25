b9753bed20d381b6eecb50bd4e01cf27e240b2cb
Afsana Akhter Khusbu <hadirifat100@gmail.com>
2023-08-26 03:48:42 +0600

    done

--- index.js
+++ index.js
@@ -1,56 +1,27 @@
 const { createStore, applyMiddleware } = require("redux");
-const {
-    delayActionMiddleware,
-    fetchTodosMiddleware,
-} = require("./middleWare");
+const {fetchMiddleware} =require('./middleWare')
+const {fetchTodos} =require('./function')
 
-// initial state
 const initialState = {
-    todos: [],
+  todo: [], 
 };
 
-// reducer
-const todoReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "todos/todoAdded":
-            return {
-                ...state,
-                todos: [
-                    ...state.todos,
-                    {
-                        title: action.payload,
-                    },
-                ],
-            };
-
-        case "todos/todoLoaded":
-            return {
-                ...state,
-                todos: [...state.todos, ...action.payload],
-            };
-
-        default:
-            break;
-    }
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "todo/loaded":
+      return {
+        ...state,
+        todo: [...state.todo, ...action.payload],
+      };
+      default:
+        return state
+  }
 };
 
-// store
-const store = createStore(
-    todoReducer,
-    applyMiddleware(delayActionMiddleware, fetchTodosMiddleware)
-);
+const store=createStore(reducer, applyMiddleware(fetchMiddleware))
 
-// subscribe to state changes
-store.subscribe(() => {
+store.subscribe(()=>{
     console.log(store.getState());
-});
+})
 
-// disptach actions
-// store.dispatch({
-//     type: "todos/todoAdded",
-//     payload: "Learn Redux from LWS",
-// });
-
-store.dispatch({
-    type: "todos/fetchTodos",
-});
+store.dispatch(fetchTodos)
--- middleWare.js
+++ middleWare.js
@@ -1,43 +1,10 @@
-const fetch = require("node-fetch");
 
-const delayActionMiddleware = (store) => (next) => (action) => {
-    if (action.type === "todos/todoAdded") {
-        console.log("I am delaying you!");
-
-        setTimeout(() => {
-            next(action);
-        }, 2000);
-
-        return;
+const fetchMiddleware=(store)=>(next)=>async(action)=>{
+    if(typeof action === "function"){
+        return action(store.dispatch, store.getState)
     }
-
-    return next(action);
-};
-
-const fetchTodosMiddleware = (store) => (next) => async (action) => {
-    if (action.type === "todos/fetchTodos") {
-        const response = await fetch(
-            "https://jsonplaceholder.typicode.com/todos?_limit=5"
-        );
-
-        const todos = await response.json();
-
-        store.dispatch({
-            type: "todos/todoLoaded",
-            payload: todos,
-        });
-
-        console.log(
-            `Number of updated todos: ${store.getState().todos.length}`
-        );
-
-        return;
-    }
-
-    return next(action);
-};
-
-module.exports = {
-    delayActionMiddleware,
-    fetchTodosMiddleware,
-};
+    return next(action)
+}
+module.exports={
+    fetchMiddleware
+}  
